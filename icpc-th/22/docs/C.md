ad3e0f4f0b6da1c50e1e6ee8f672cb0ae89461b5
Alireza Ghasemi <agcombest@gmail.com>
2023-04-21 02:05:48 +0330

    Add ICPC TH 22 C solution draft

---
+++ icpc-th/22/docs/C.md
@@ -0,0 +1,85 @@
+# C
+
+> Limits are self-assigned based on the minimal found solution.
+
+- Time limit: 2 seconds
+- Memory limit: 256 MB
+- Accepts multiple outputs as correct solution per test-case.
+- A correct output per test-case must include only less than 12000 moves/swaps.
+
+## Simplify
+
+Sort a given array with limited swap operations; only rooms with indexes differing in one bit-position are allowed to be swapped together.
+
+## Think
+
+I believe there exists a simple solution for this based on customizing a sorting algorithm.
+
+### Customized bubble-sort
+
+We have
+
+```java
+private static boolean areOneBitPosDiff(final int i, final int j) {
+	final int xor = i ^ j;
+	return xor != 0 && (xor & (xor - 1)) == 0;
+}
+
+private void immediateSwap(final int i, final int j) {
+	final int t = as[i];
+	as[i] = as[j];
+	as[j] = t;
+}
+```
+
+; the `areOneBitPosDiff` relies on the fact that `xor-1` causes the right-most set bit to flip and creates a cascading wave of flips to its right bits (do a generic example with `xxx1000` to digest it); and the glue part
+
+```java
+for (int i = 0; i < as.length - 1; i++) {
+	for (int j = 0; j < as.length - i - 1; j++) {
+		if (as[j] > as[j + 1] && areOneBitPosDiff(j, j + 1)) {
+			immediateSwap(j, j + 1);
+		}
+	}
+}
+```
+
+Let us give a contradicting input that won't get sorted by this: `1 3 2 4`; it can not swap numbers 3 and 2 because of the limitation, more precisely said, it can not do an immediate swap there; to swap indexes 1 and 2, it needs to use the intermediate index 0; swap indexes 1 and 0, then indexes 0 and 2, but this causes numbers 2 and 1 to be misplaced which can be resolved by swapping indexes 1 and 0; so, there should be a logic to handle non-immediate swaps.
+
+### Graph of possible swaps
+
+I did not go more deep on customizing a sorting algorithm to solve this problem; I hopped into graphs as soon as its light bulb turned on, and let us see if it works.
+
+- Construct a graph for the indexes, with edges connecting those that differ in one bit-position.
+- Sort a copy of the array.
+- Compare the sorted array with the original one to figure out the needed swaps.
+- For each needed swap like `(u, v)`: 
+  - Find the shortest path between nodes/indexes `u` and `v`, like `(u, a, b, ..., y, z, v)`.
+  - Do a smart swap by moving on the path:
+    - Immediate swap `(u, a)`.
+    - Immediate swap `(a, b)`.
+    - ...
+    - Immediate swap `(y, z)`.
+    - Immediate swap `(z, v)`.
+  - Finally, the `u` and `v` are swapped, but everything in the way got a shift; recover from the shift:
+    - Traverse the path in reverse, skip the first, and do immediate swaps:
+      - Immediate swap `(y, z)`.
+      - ...
+      - Immediate swap `(a, b)`.
+      - Immediate swap `(u, a)`.
+  - Sorted.
+
+Let us analyze the solution:
+- Constructing the graph happens in `O((2^n)^2)` (max `n` is `10`).
+- Sorting a copy of the array happens in `O((2^n)*log(2^n)) = O((2^n)*n)`.
+- Finding the shortest path between two nodes can happen by a BFS (with graph edges stored in the adjacency list format) in `O((2^n)+C((2^n), 2))` (maximum number of edges is actually pretty low, 10240).
+- Each smart swap, beside finding a shortest path, costs `O(C((2^n), 2))`.
+- Just being lucky not getting over 12000 moves limit (actually, it may not be luck preventing that; there might be a calculable hard-limit for our solution).
+
+## Code
+
+See [`./../src/C.java`](./../src/C.java)
+
+## Over-engineering
+
+> Hard-coded adjacency list...
---
+++ icpc-th/22/src/C.java
@@ -0,0 +1,222 @@
+import java.io.*;
+import java.util.*;
+
+public final class C {
+	
+	private static final FastNonBlankReader in = new FastNonBlankReader(new BufferedReader(new InputStreamReader(System.in))); // 16 KB default buffer
+	private static final PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out))); // 16 KB default buffer
+	
+	public static void main(final String[] args) {
+		final int n = in.nextInt();
+		final int[] as = new int[1 << n];
+		for (int i = 0; i < as.length; i++) {
+			as[i] = in.nextInt();
+		}
+		
+		final List<Edge> swaps = new C(as).computeSwaps();
+		out.println(swaps.size());
+		for (Edge swap : swaps) {
+			out.println(swap.u + " " + swap.v);
+		}
+		
+		out.flush(); // Mandatory
+	}
+	
+	private final int[] as;
+	
+	private final ArrayList<Integer>[] adjacency;
+	private final int[] asSorted;
+	private final Map<Integer, Integer> asReverse;
+	private final List<Edge> swaps;
+	
+	public C(final int[] as) {
+		this.as = as;
+		
+		// Init adj
+		adjacency = new ArrayList[as.length];
+		for (int i = 0; i < adjacency.length; i++) {
+			adjacency[i] = new ArrayList<>(adjacency.length);
+		}
+		
+		for (int i = 0; i < as.length - 1; i++) {
+			for (int j = i + 1; j < as.length; j++) {
+				if (areOneBitPosDiff(i, j)) {
+					adjacency[i].add(j);
+					adjacency[j].add(i);
+				}
+			}
+		}
+		
+		System.out.println(Arrays.stream(adjacency).mapToLong(Collection::size).sum());
+		
+		// Init asSorted
+		asSorted = Arrays.copyOf(as, as.length);
+		Arrays.sort(as);
+		
+		// Init asRev
+		asReverse = new HashMap<>(as.length * 2);
+		for (int i = 0; i < as.length; i++) {
+			final int a = as[i];
+			asReverse.put(a, i);
+		}
+		
+		// Init swaps
+		swaps = new LinkedList<>();
+	}
+	
+	private List<Edge> computeSwaps() {
+		for (int i = 0; i < as.length; i++) {
+			final int j = asReverse.get(asSorted[i]);
+			if (i != j) {
+				smartSwap(i, j);
+			}
+		}
+		
+		assert isSorted(as);
+		assert swaps.size() < 12000;
+		
+		return swaps;
+	}
+	
+	private static boolean isSorted(final int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private void smartSwap(final int i, final int j) {
+		final LinkedList<Edge> shortestPathRes = findShortestPath(i, j);
+		for (final Edge e : shortestPathRes) {
+			immediateSwap(e.u, e.v);
+			swaps.add(e);
+		}
+		
+		final Iterator<Edge> shortestPathResRevIt = shortestPathRes.descendingIterator();
+		shortestPathResRevIt.next();
+		while (shortestPathResRevIt.hasNext()) {
+			final Edge e = shortestPathResRevIt.next();
+			immediateSwap(e.u, e.v);
+			swaps.add(e);
+		}
+	}
+	
+	private LinkedList<Edge> findShortestPath(final int start, final int end) {
+		final int[] parents = new int[adjacency.length];
+		Arrays.fill(parents, -1);
+		parents[start] = start;
+		
+		final boolean[] visiteds = new boolean[adjacency.length];
+		visiteds[start] = true;
+		
+		final Queue<Integer> queue = new LinkedList<>();
+		queue.add(start);
+		
+		while (!queue.isEmpty()) {
+			final int current = queue.poll();
+			
+			for (final int neighbor : adjacency[current]) {
+				if (!visiteds[neighbor]) {
+					visiteds[neighbor] = true;
+					parents[neighbor] = current;
+					queue.add(neighbor);
+				}
+			}
+		}
+		
+		final LinkedList<Edge> shortestPath = new LinkedList<>();
+		int current = end;
+		while (current != start) {
+			final int parent = parents[current];
+			shortestPath.addFirst(new Edge(parent, current));
+			current = parent;
+		}
+		
+		return shortestPath;
+	}
+	
+	private void immediateSwap(final int i, final int j) {
+		final int t = as[i];
+		as[i] = as[j];
+		as[j] = t;
+		
+		asReverse.put(as[i], i);
+		asReverse.put(as[j], j);
+	}
+	
+	private static boolean areOneBitPosDiff(final int i, final int j) {
+		final int xor = i ^ j;
+		return xor != 0 && ((xor) & (xor - 1)) == 0;
+	}
+	
+	private static final class Edge {
+		public final int u, v;
+		
+		public Edge(final int u, final int v) {
+			this.u = u;
+			this.v = v;
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			else if (o == null || getClass() != o.getClass()) return false;
+			
+			final Edge other = (Edge) o;
+			return (u == other.u && v == other.v) || (v == other.u && u == other.v);
+		}
+	}
+	
+	private static final class FastNonBlankReader implements Closeable {
+		private final BufferedReader br;
+		private StringTokenizer st;
+		
+		private FastNonBlankReader(final BufferedReader bufferedReader) {
+			br = bufferedReader;
+			st = new StringTokenizer("");
+		}
+		
+		public String nextLine() {
+			if (st.hasMoreTokens()) {
+				final StringBuilder sb = new StringBuilder();
+				do {
+					sb.append(st.nextToken());
+				} while (st.hasMoreTokens());
+				return sb.toString();
+			} else return readLine();
+		}
+		
+		public String next() {
+			while (!st.hasMoreTokens()) {
+				st = new StringTokenizer(readLine());
+			}
+			return st.nextToken();
+		}
+		
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		private String readLine() {
+			try {
+				return br.readLine();
+			} catch (final IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		}
+		
+		@Override
+		public void close() {
+			st = null;
+			try {
+				br.close();
+			} catch (final IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		}
+	}
+	
+}
